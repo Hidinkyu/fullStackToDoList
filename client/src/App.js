53404caf93f0e7f138b26f1e6d5843a29ec10560
roy bae <roybae@roys-Laptop.home>
2022-11-18 19:01:33 -0800

    Feature - Able to add delete and complete tasks

--- client/src/App.js
+++ client/src/App.js
@@ -19,9 +19,9 @@ const App = () => {
 	}
 
 	const completeTodo = async id => {
-		const data = fetch(API_BASE + `/todo/complete/${id}`)
-		.then(res => res.json());
-		console.log(data);
+		const data = await fetch(API_BASE + `/todo/complete/${id}`, {
+			method: 'PUT'
+		}).then(res => res.json());
 
 		setTodos(todos => todos.map(todo => {
 			if (todo._id === data._id) todo.complete = data.complete;
@@ -29,22 +29,61 @@ const App = () => {
 		}))
 	}
 
+	const deleteTodo = async id => {
+		const data = await fetch(API_BASE + `/todo/delete/${id}`, {
+			method: "DELETE"
+		}).then(res => res.json());
+		setTodos(todos => todos.filter(todo => todo._id !== data._id));
+	}
+
+	const addTodo = async () => {
+		if (newTodo) {const data = await fetch(API_BASE + `/todo/new`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				text: newTodo
+			})
+		}).then(res => res.json()); 
+		setTodos([...todos, data]);
+		setPopupActive(false);
+		setNewTodo('');}
+	}
+
 	return (
 		<div className="App">
-			<h1>Welcome!</h1>
-			<h4>Your Tasks</h4>
+			<h1 className="noselect">Welcome!</h1>
+			<h4 className='noselect'>Your Tasks</h4>
 
 			<div className="todos">
 				{todos.map(todo => (
-				<div className={"todo " + (todo.complete ? "is-complete" : '')} key={todo._id} onClick={() => completeTodo(todo._id)}>
+					<div className="todoBox" key={todo._id}>
+				<div className={"todo " + (todo.complete ? "is-complete" : '')} onClick={() => completeTodo(todo._id)}>
 					<div className="checkBox"></div>
 
 					<div className="text">{todo.text}</div>
 
-					<div className="delete-todo">X</div> 
 				</div>
+					<div className="delete-todo noselect" onClick={() => deleteTodo(todo._id)}>X</div> 
+					</div>
 				))}
 			</div>
+			<div className="addPopup noselect" onClick={() => setPopupActive(true)}> + </div>
+
+			{ popupActive ? 
+			(
+			<div className='popup'>
+				<div className='closePopup noselect' onClick={()=> setPopupActive(false)}>X</div>
+				<h3 className='noselect'>Add Task</h3>
+				<input 
+				type="text" 
+				className="add-todo-input" 
+				onChange={e => setNewTodo(e.target.value)}
+				value={newTodo}/>
+				<div className="button noselect" onClick={ addTodo }>Create Task</div>
+			</div>
+				) : '' }
 		</div>
 	);
 }
